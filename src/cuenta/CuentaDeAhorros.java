f758225f1d636aad9618d62043ed9daf2d778be3
Bryan Lopez <blopezav7220@universidadean.edu.co>
2017-08-26 11:27:46 -0500

    Apliación cuenta de ahorro, con las indicaciones necesarias para calcular los intereses mes a mes

--- src/cuenta/CuentaDeAhorros.java
+++ src/cuenta/CuentaDeAhorros.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cuenta;
 
 /**
@@ -53,5 +48,4 @@ public class CuentaDeAhorros {
             System.out.println("El saldo del mes " + i + " es: " + resultado);
         }
     }
-
 }
